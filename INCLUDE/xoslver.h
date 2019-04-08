fc5d50fa4bd7ee6037a20f105642bc714d1977d5
norman <norman@thebacks.co.uk>
2019-04-08 08:45:38 +0100

    XOSL130a22

--- INCLUDE/xoslver.h
+++ INCLUDE/xoslver.h
@@ -11,7 +11,7 @@
 #ifndef __xoslversion__
 #define __xoslversion__
 
-#define XOSL_VERSION "1.3.0a21"
-#define XOSL_LABEL "XOSL130a21 "
+#define XOSL_VERSION "1.3.0a22"
+#define XOSL_LABEL "XOSL130a22 "
 
 #endif
